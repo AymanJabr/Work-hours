052175f0596c5c9b15eca171958b6ff9985ada4d
AymanJabr <aymanjaber2012@gmail.com>
2021-04-24 00:14:08 +0300

    Improve efficiency of monthly reports

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
   end
 
   def monthly_report
-    @groups = Group.all.map { |group| group if group.workhours.any? }.compact
+    @groups = Group.all.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).map { |group| group if group.workhours.any? }.compact
   end
 
   def show
