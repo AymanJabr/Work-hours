fb5e9cd9bd8299f53ca761491474e230665b93f0
AymanJabr <aymanjaber2012@gmail.com>
2021-04-25 14:08:41 +0300

    Add initial nav css file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,21 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+console.log("Does this work")
+
+function myFunction() {
+    console.log("Entered in myFunction")
+    
+    document.getElementById("myDropdown").classList.toggle("show-dropdown");
+}
+
+
+window.onclick = function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show-dropdown')) {
+            myDropdown.classList.remove('show-dropdown');
+        }
+    }
+}
