524970d109a1b7fdca95bcce55496f561f3aa71e
AymanJabr <aymanjaber2012@gmail.com>
2021-04-26 00:02:29 +0300

    Modified My-transactions page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,11 @@ ActiveStorage.start()
 
 window.onclick = function (e) {
     if (!e.target.matches('.dropbtn') && !e.target.matches('.fa-bars')) {
-        console.log("remove the show-dropdown class")
         var myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show-dropdown')) {
             myDropdown.classList.remove('show-dropdown');
         }
     } else {
-        console.log("Entered in myFunction")
         document.getElementById("myDropdown").classList.toggle("show-dropdown");
     }
 }
