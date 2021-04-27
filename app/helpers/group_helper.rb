ddef743e19a366cc6814b03972bdba24d9e2f988
AymanJabr <aymanjaber2012@gmail.com>
2021-04-28 00:47:52 +0300

    Add Group Show Helper

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -1,2 +1,20 @@
 module GroupHelper
+
+    def show_all_group_workhours(group)
+        all_contents = content_tag(:br)
+        group.workhours.each do |workhour|
+            all_contents << content_tag(:div, class: "information") do
+                inside_content = content_tag(:div, workhour.first_group_icon, class: "information-image")
+                inside_content << content_tag(:div, workhour.name, class: "information-name")
+                inside_content << content_tag(:div, workhour.amount, class: "information-amount")
+                inside_content << content_tag(:div, workhour.created_at, class: "information-date")
+                inside_content << content_tag(:div, (link_to "Edit Workhours", edit_workhour_path(workhour), class: "my-edit-button"), class: "edit-button-container") 
+                inside_content
+            end
+        end
+        all_contents
+    end
+
+
+
 end
