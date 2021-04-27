bcb0d9e3bc47f7c329c5c4aa18fa7fe48b167715
AymanJabr <aymanjaber2012@gmail.com>
2021-04-28 01:39:57 +0300

    Add Helpers for Workhours

--- app/helpers/workhour_helper.rb
+++ app/helpers/workhour_helper.rb
@@ -1,2 +1,20 @@
 module WorkhourHelper
+
+    def show_correct_workhours(workhours)
+        all_contents = content_tag(:br)
+        workhours.each do |workhour|
+            all_contents << content_tag(:div, class: 'information') do
+                inside_content = content_tag(:div, workhour.first_group_icon, class: 'information-image')
+                inside_content << content_tag(:div, workhour.name, class: 'information-name')
+                inside_content << content_tag(:div, workhour.amount, class: 'information-amount')
+                inside_content << content_tag(:div, workhour.created_at, class: 'information-date')
+                inside_content << content_tag(:div,
+                                        (link_to 'Edit Workhours', edit_workhour_path(workhour), class: 'my-edit-button'), class: 'edit-button-container')
+                inside_content
+            end
+        end
+        all_contents
+    end
+    
+
 end
