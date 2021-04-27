e99d88fa3f1e221ece6155b2efeeb86b40011c7b
AymanJabr <aymanjaber2012@gmail.com>
2021-04-27 22:44:31 +0300

    Fix some linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ApplicationRecord
   has_many :workhours, through: :group_workhours
 
   validates :name, presence: true, minimum: 5, allow_blank: true
-  validates :icon, presence: true, format: URI::regexp(%w[http https])
+  validates :icon, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 
   scope :in_order, -> { order(created_at: :desc) }
   scope :in_alphabetical_order, -> { order(name: :asc) }
